366c25ded52788ed00b0d9d008736eb268b04358
Lucas A. L. Theodoro <lucas.alt40@gmail.com>
2023-12-17 19:13:03 -0300

    Iniciando o projeto

---
+++ src/main/java/com/projetouirapuru/hoteluirapuru/HotelUirapuruApplication.java
@@ -0,0 +1,13 @@
+package com.projetouirapuru.hoteluirapuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelUirapuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelUirapuruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projetouirapuru/hoteluirapuru/HotelUirapuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetouirapuru.hoteluirapuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelUirapuruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
